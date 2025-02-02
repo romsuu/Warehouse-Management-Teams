2c8362fe63c4be736e04e9b0f618a9d2a1e22003
romsuu <roman.jastrebov@gmail.com>
2025-02-02 17:56:43 +0200

    Update auth.php

--- php/auth.php
+++ php/auth.php
@@ -1,8 +1,15 @@
 <?php
 session_start();
+
+// Check if the logout parameter is set
 if (isset($_GET['logout'])) {
+    // Unset all session variables
     session_unset();
+    
+    // Destroy the session
     session_destroy();
+    
+    // Redirect to the index page
     header("Location: /index.php");
     exit;
 }
